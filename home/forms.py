f33bec58eff54c42b9730b6296860b549cedd302
SarfasMohdHaathim <smhkc2001@gmail.com>
2023-01-13 19:59:30 +0530

    MODELS Error Corrected

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,17 @@
 from django.forms import ModelForm,forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from . models import Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','password1','password2']
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model=Profile
+        fields='__all__'
 
